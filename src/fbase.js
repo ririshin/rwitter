93348526e5ad7cb33de7272cb4934316b1291283
ririshin <hyerishin0@gmail.com>
2024-01-12 12:07:38 +0900

    인증 모듈 추가

--- src/fbase.js
+++ src/fbase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -13,7 +10,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_API_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 console.log(app);
